6932296478c93577b000ad6491874c3c320c0e5c
Mikael Berg <miberg@silabs.com>
2015-11-15 16:17:03 +0100

    test(lib): Respects last and after and before

--- test/index.js
+++ test/index.js
@@ -370,16 +370,90 @@ describe('connectionFromArray()', () => {
       });
     });
 
-    it('respects last and after and before, too few', () => {
-      assert(false, 'Not yet implemented');
+    it('respects last and after and before, too few', async () => {
+      const c = await connectionFromMongo(findAll, {
+        last: 2,
+        after: 'bW9uZ29kYmNvbm5lY3Rpb246MA==',
+        before: 'bW9uZ29kYmNvbm5lY3Rpb246NA==',
+      });
+      expect(c).to.deep.equal({
+        edges: [
+          {
+            node: { letter: 'C', _id: 'letter_C' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mg==',
+          },
+          {
+            node: { letter: 'D', _id: 'letter_D' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          },
+        ],
+        pageInfo: {
+          startCursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mg==',
+          endCursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          hasPreviousPage: true,
+          hasNextPage: false,
+        },
+      });
     });
 
-    it('respects last and after and before, too many', () => {
-      assert(false, 'Not yet implemented');
+    it('respects last and after and before, too many', async () => {
+      const c = await connectionFromMongo(findAll, {
+        last: 4, // different from graphql-relay-js
+        after: 'bW9uZ29kYmNvbm5lY3Rpb246MA==',
+        before: 'bW9uZ29kYmNvbm5lY3Rpb246NA==',
+      });
+      expect(c).to.deep.equal({
+        edges: [
+          {
+            node: { letter: 'B', _id: 'letter_B' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246MQ==',
+          },
+          {
+            node: { letter: 'C', _id: 'letter_C' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mg==',
+          },
+          {
+            node: { letter: 'D', _id: 'letter_D' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          },
+        ],
+        pageInfo: {
+          startCursor: 'bW9uZ29kYmNvbm5lY3Rpb246MQ==',
+          endCursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          hasPreviousPage: false,
+          hasNextPage: false,
+        },
+      });
     });
 
-    it('respects last and after and before, exactly right', () => {
-      assert(false, 'Not yet implemented');
+    it('respects last and after and before, exactly right', async () => {
+      const c = await connectionFromMongo(findAll, {
+        last: 3,
+        after: 'bW9uZ29kYmNvbm5lY3Rpb246MA==',
+        before: 'bW9uZ29kYmNvbm5lY3Rpb246NA==',
+      });
+      expect(c).to.deep.equal({
+        edges: [
+          {
+            node: { letter: 'B', _id: 'letter_B' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246MQ==',
+          },
+          {
+            node: { letter: 'C', _id: 'letter_C' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mg==',
+          },
+          {
+            node: { letter: 'D', _id: 'letter_D' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          },
+        ],
+        pageInfo: {
+          startCursor: 'bW9uZ29kYmNvbm5lY3Rpb246MQ==',
+          endCursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          hasPreviousPage: false,
+          hasNextPage: false,
+        },
+      });
     });
   });
 
