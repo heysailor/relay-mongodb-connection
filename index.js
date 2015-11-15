14302057e5692f6b1bba423301e402c905574706
Mikael Berg <miberg@silabs.com>
2015-11-15 14:13:55 +0100

    feat(pkg): Returns all elements without filters

--- index.js
+++ index.js
@@ -1,2 +1,63 @@
+const PREFIX = 'mongodbconnection:';
+
 export const base64 = (str) => (new Buffer(str, 'ascii')).toString('base64');
 export const unbase64 = (b64) => (new Buffer(b64, 'base64')).toString('ascii');
+
+/**
+ * Creates the cursor string from an offset.
+ */
+export function offsetToCursor(offset) {
+  return base64(PREFIX + offset);
+}
+
+/**
+ * Accepts a mongodb cursor and connection arguments, and returns a connection
+ * object for use in GraphQL. It uses array offsets as pagination, so pagiantion
+ * will work only if the data set is satic.
+ */
+export async function connectionFromMongo(mongodbCursor, args) {
+  const startOffset = 0;
+
+  const slice = await mongodbCursor.toArray();
+
+  const edges = slice.map((value, index) => ({
+    cursor: offsetToCursor(startOffset + index),
+    node: value,
+  }));
+
+  const firstEdge = edges[0];
+  const lastEdge = edges[edges.length - 1];
+
+  return {
+    edges,
+    pageInfo: {
+      startCursor: firstEdge ? firstEdge.cursor : null,
+      endCursor: lastEdge ? lastEdge.cursor : null,
+      hasNextPage: await mongodbCursor.hasNext(),
+      hasPreviousPage: false, // Not yet implemented
+    },
+  };
+}
+
+/**
+ * Rederives the offset from the cursor string
+ */
+export function cursorToOffset(cursor) {
+  return parseInt(unbase64(cursor).substring(PREFIX.length), 10);
+}
+
+/**
+ * Return the cursor associated with an object in an array
+ */
+export function cursorForObjectInConnection(data, object) {
+  // NYI
+}
+
+/**
+ * Given an optional cursor and a default offset, returns the offset to use;
+ * if the cursor contains a valid offset, that will be used, otherwise it will
+ * be the default.
+ */
+export function getOffsetWithDefault(cursor, defaultOffset) {
+  // NYI
+}
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 import { assert, expect } from 'chai';
-import { base64, unbase64 } from '../index';
+import { base64, unbase64, connectionFromMongo } from '../index';
 import { connect } from './db';
 
 const COL = 'letters';
@@ -14,8 +14,9 @@ describe('connectionFromArray()', () => {
       done(e);
     }
 
+    await db.collection(COL).drop();
     await db.collection(COL).insertMany(
-      ['A', 'B', 'C', 'D', 'E'].map(letter => ({ letter }))
+      ['A', 'B', 'C', 'D', 'E'].map(letter => ({ letter, _id: `letter_${letter}` }))
     );
 
     done();
@@ -26,8 +27,43 @@ describe('connectionFromArray()', () => {
   });
 
   describe('basic slicing', () => {
-    it('returns all elements without filters', () => {
-      assert(false, 'Not yet implemented');
+    let findAll;
+    beforeEach(() => {
+      findAll = db.collection(COL).find({});
+    });
+
+    it('returns all elements without filters', async () => {
+      const c = await connectionFromMongo(findAll);
+      expect(c).to.deep.equal({
+        edges: [
+          {
+            node: { letter: 'A', _id: 'letter_A' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246MA==',
+          },
+          {
+            node: { letter: 'B', _id: 'letter_B' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246MQ==',
+          },
+          {
+            node: { letter: 'C', _id: 'letter_C' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mg==',
+          },
+          {
+            node: { letter: 'D', _id: 'letter_D' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          },
+          {
+            node: { letter: 'E', _id: 'letter_E' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246NA==',
+          },
+        ],
+        pageInfo: {
+          startCursor: 'bW9uZ29kYmNvbm5lY3Rpb246MA==',
+          endCursor: 'bW9uZ29kYmNvbm5lY3Rpb246NA==',
+          hasPreviousPage: false,
+          hasNextPage: false,
+        },
+      });
     });
 
     it('respects a smaller first', () => {
