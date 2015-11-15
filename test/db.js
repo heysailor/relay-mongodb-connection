0fe03e09ac7d78daeb16a83114493e087fae5c35
Mikael Berg <miberg@silabs.com>
2015-11-15 17:19:38 +0100

    chore(travis): New strategy for connecting to test db

--- test/db.js
+++ test/db.js
@@ -1,9 +1,5 @@
 import { MongoClient } from 'mongodb';
 
 export const connect = async () => {
-  if (!process.env.MONGO_CONNECTION_STRING) {
-    throw new Error(`Environment variable MONGO_CONNECTION_STRING must be set`);
-  }
-
   return MongoClient.connect(process.env.MONGO_CONNECTION_STRING);
 };
