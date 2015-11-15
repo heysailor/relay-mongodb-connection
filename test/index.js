0a77867e8cc5aa2cd3ed94339953aadab9e0e55f
Mikael Berg <miberg@silabs.com>
2015-11-15 16:24:57 +0100

    test(lib): Returns all elements if cursors are invalid

--- test/index.js
+++ test/index.js
@@ -471,8 +471,41 @@ describe('connectionFromArray()', () => {
       });
     });
 
-    it('returns all elements if cursors are invalid', () => {
-      assert(false, 'Not yet implemented');
+    it('returns all elements if cursors are invalid', async () => {
+      const c = await connectionFromMongo(findAll, {
+        before: 'invalid',
+        after: 'invalid',
+      });
+      expect(c).to.deep.equal({
+        edges: [
+          {
+            node: { letter: 'A', _id: 'letter_A' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246MA==',
+          },
+          {
+            node: { letter: 'B', _id: 'letter_B' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246MQ==',
+          },
+          {
+            node: { letter: 'C', _id: 'letter_C' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mg==',
+          },
+          {
+            node: { letter: 'D', _id: 'letter_D' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246Mw==',
+          },
+          {
+            node: { letter: 'E', _id: 'letter_E' },
+            cursor: 'bW9uZ29kYmNvbm5lY3Rpb246NA==',
+          },
+        ],
+        pageInfo: {
+          startCursor: 'bW9uZ29kYmNvbm5lY3Rpb246MA==',
+          endCursor: 'bW9uZ29kYmNvbm5lY3Rpb246NA==',
+          hasPreviousPage: false,
+          hasNextPage: false,
+        },
+      });
     });
 
     it('returns all elements if cursors are on the outside', () => {
