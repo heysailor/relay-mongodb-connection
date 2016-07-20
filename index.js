684d2a99674a7980fe661773e1af995bae290a27
Gershon Papiashvili <papigers93@gmail.com>
2016-07-20 09:23:04 +0300

    Added support for Mongoose
    I've saw
    [`relay-mongodb-connection`](https://github.com/mikberg/relay-mongodb-connection),
    loved it and wanted to do the same for mongoose users.
    Most of the code is basically the same, just few changes to match
    mongoose API.
    
    Added the function `connectionFromMongooseQuery` and
    `connectionFromMongooseAggregate` to create a connection from mongoose
    `Query` or `Aggregate` respectively.
    
    Didn't want to open an individual package, since most of the code is the
    same and NOT MINE.
    
    Haven't done much testing, just enough to cover all my use cases.

--- index.js
+++ index.js
@@ -87,3 +87,128 @@ export default async function connectionFromMongoCursor(inMongoCursor, args = {}
     },
   };
 }
+
+export async function connectionFromMongooseQuery(query, args = {}, mapper) {
+  const mongooseQuery = query;
+  const { after, before, first, last } = args;
+  const count = await mongooseQuery.count();
+  const beforeOffset = getOffsetWithDefault(before, count);
+  const afterOffset = getOffsetWithDefault(after, -1);
+  
+  let startOffset = Math.max(-1, afterOffset) + 1;
+  let endOffset = Math.min(count, beforeOffset);
+  
+  if (first !== undefined) {
+    endOffset = Math.min(endOffset, startOffset + first);
+  }
+  
+  if (last !== undefined) {
+    startOffset = Math.min(startOffset, endOffset - last);
+  }
+  
+  const skip = Math.max(startOffset, 0);
+  const limit = endOffset - startOffset;
+  
+  mongooseQuery.skip(skip);
+  mongooseQuery.limit(limit);
+  
+  let slice;
+  if (limit === 0) {
+    slice = [];
+  }
+  else {
+    const res = await mongooseQuery.find();
+    slice = res;
+  }
+  
+  if (typeof mapper === 'function') {
+    slice = slice.map(mapper);
+  }
+  
+  const edges = slice.map((value, index) => ({
+    cursor: offsetToCursor(startOffset + index),
+    node: value,
+  }));
+  
+  const firstEdge = edges[0];
+  const lastEdge = edges[edges.length - 1];
+  const lowerBound = after ? (afterOffset + 1) : 0;
+  const upperBound = before ? Math.min(beforeOffset, count) : count;
+  
+  return {
+    edges,
+    pageInfo: {
+      startCursor: firstEdge ? firstEdge.cursor : null,
+      endCursor: lastEdge ? lastEdge.cursor : null,
+      hasPreviousPage: last !== null ? startOffset > lowerBound : false,
+      hasNextPage: first !== null ? endOffset < upperBound : false,
+    },
+  };
+}
+
+function cloneAggregation(aggr) {
+  /* eslint-disable no-underscore-dangle */
+  const model = aggr._model.model(aggr._model.modelName);
+  return model.aggregate(aggr._pipeline);
+  /* eslint-enable no-underscore-dangle */
+}
+
+export async function connectionFromMongooseAggregate(aggr, args = {}, mapper) {
+  const mongooseAggr = cloneAggregation(aggr);
+  const countAggr = cloneAggregation(aggr);
+  const { after, before, first, last } = args;
+  const countArr = await countAggr.group({ _id: null, count: { $sum: 1 } });
+  const count = countArr.length > 0 && countArr[0].count ? countArr[0].count : 0;
+  const beforeOffset = getOffsetWithDefault(before, count);
+  const afterOffset = getOffsetWithDefault(after, -1);
+  
+  let startOffset = Math.max(-1, afterOffset) + 1;
+  let endOffset = Math.min(count, beforeOffset);
+  
+  if (first !== undefined) {
+    endOffset = Math.min(endOffset, startOffset + first);
+  }
+  
+  if (last !== undefined) {
+    startOffset = Math.min(startOffset, endOffset - last);
+  }
+  
+  const skip = Math.max(startOffset, 0);
+  const limit = endOffset - startOffset;
+  
+  mongooseAggr.skip(skip);
+  mongooseAggr.limit(limit);
+  
+  let slice;
+  if (limit === 0) {
+    slice = [];
+  }
+  else {
+    const res = await mongooseAggr.exec();
+    slice = res;
+  }
+  
+  if (typeof mapper === 'function') {
+    slice = slice.map(mapper);
+  }
+  
+  const edges = slice.map((value, index) => ({
+    cursor: offsetToCursor(startOffset + index),
+    node: value,
+  }));
+  
+  const firstEdge = edges[0];
+  const lastEdge = edges[edges.length - 1];
+  const lowerBound = after ? (afterOffset + 1) : 0;
+  const upperBound = before ? Math.min(beforeOffset, count) : count;
+  
+  return {
+    edges,
+    pageInfo: {
+      startCursor: firstEdge ? firstEdge.cursor : null,
+      endCursor: lastEdge ? lastEdge.cursor : null,
+      hasPreviousPage: last !== null ? startOffset > lowerBound : false,
+      hasNextPage: first !== null ? endOffset < upperBound : false,
+    },
+  };
+}
