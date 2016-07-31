fbe586008c04db64823742b6c38aacc5bf2a3ab0
Mikael Berg <mikael@zeppelin.no>
2016-07-31 20:02:41 +0200

    use mongoose's promise to simplify mongoose aggregate

--- src/connectionFromMongooseAggregate.js
+++ src/connectionFromMongooseAggregate.js
@@ -1,47 +1,40 @@
+'use strict';
+
 var utils = require('./utils');
 var getOffsetsFromArgs = utils.getOffsetsFromArgs;
 var getConnectionFromSlice = utils.getConnectionFromSlice;
 
-var cloneAggregation = function cloneAggregation(aggr) {
+function cloneAggregation(aggr) {
   /* eslint-disable no-underscore-dangle */
   var model = aggr._model.model(aggr._model.modelName);
   return model.aggregate(aggr._pipeline);
   /* eslint-enable no-underscore-dangle */
 }
 
-module.exports = exports = function connectionFromMongooseAggregate(aggr, args, mapper) {
-  args = args === undefined ? {} : args;
+function connectionFromMongooseAggregate(aggr, inArgs, mapper) {
+  var args = inArgs || {};
   var mongooseAggr = cloneAggregation(aggr);
   var countAggr = cloneAggregation(aggr);
-  
-  return new Promise(function (resolve, reject) {
-    countAggr
-      .group({ _id: null, count: { $sum: 1 } })
-      .exec(function (err, countArr) {
-        /* istanbul ignore if  */
-        if (err) {
-          return reject(err);
-        }
 
-        var count = countArr.length > 0 && countArr[0].count ? countArr[0].count : 0;
+  return countAggr
+    .group({ _id: null, count: { $sum: 1 } })
+    .exec()
+    .then(function countPromise(countArr) {
+      var count = countArr.length > 0 && countArr[0].count ? countArr[0].count : 0;
+      var pagination = getOffsetsFromArgs(args, count);
 
-        var mongoPaginationArgs = getOffsetsFromArgs(args, count);
-        var skip = mongoPaginationArgs.skip;
-        var limit = mongoPaginationArgs.limit;
+      if (pagination.limit <= 0) {
+        return getConnectionFromSlice([], mapper, args, count);
+      }
 
-        if (limit <= 0) {
-          return resolve(getConnectionFromSlice([], mapper, args, count));
-        }
+      mongooseAggr.skip(pagination.skip);
+      mongooseAggr.limit(pagination.limit);
 
-        mongooseAggr.skip(skip);
-        mongooseAggr.limit(limit);
-        mongooseAggr.exec(function (err, slice) {
-          /* istanbul ignore if  */
-          if (err) {
-            return reject(err);
-          }
-          return resolve(getConnectionFromSlice(slice, mapper, args, count));
-        })
+      return mongooseAggr.exec()
+        .then(function fromSlice(slice) {
+          return getConnectionFromSlice(slice, mapper, args, count);
+        });
     });
-  });
-};
+}
+
+module.exports = connectionFromMongooseAggregate;
